5f93a46c78171720867f198cf1fdb2178933d9d1
John Hobbs <john@velvetcache.org>
2026-03-17 22:08:43 -0500

    Use itty-router and web crypto

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -1,33 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { AutoRouter } from 'itty-router'
+import { env } from "cloudflare:workers";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const buffer = await request.arrayBuffer();
-		const dv = new DataView(buffer)
-		var version = dv.getUint32(0, true);
-		var nonce = dv.getUint32(4, true);
-		var error = dv.getInt32(8, true);
+const key = await crypto.subtle.importKey(
+	"raw",
+	Uint8Array.fromBase64(env.HMAC_SECRET),
+	{ name: "HMAC", hash: {name: "SHA-256"} },
+	false,
+	["verify"]
+);
+
+const router = AutoRouter()
+
+router.get('/', async () => {
+	return new Response("Hi, yes, hello.");
+});
+
+router.post('/update', async (request, env, ctx): Promise<Response> => {
+	const buffer = await request.arrayBuffer();
+	const dv = new DataView(buffer, 0, 20)
+
+	var version = dv.getUint32(0, true);
+	if(version != 1) {
+		return new Response("Unsupported version", { status: 400 });
+	}
+
+	// var nonce = dv.getUint32(4, true);
+	var error = dv.getInt32(8, true);
+	if(error != 0) {
+		console.error("Device reported error code: " + error);
+	} else {
 		var temperature = Math.floor(dv.getFloat32(12, true) * 100)/100;
 		var humidity = Math.floor(dv.getFloat32(16, true) * 100)/100;
-		var signature = buffer.slice(20);
+		var expectedSignature = new DataView(buffer, 20); // buffer.slice(20);
+
+		const hmacValid = await crypto.subtle.verify("HMAC", key, expectedSignature, buffer.slice(0, 20));
+
+		if (!hmacValid) {
+			return new Response("Invalid HMAC signature", { status: 400 });
+		}
 
 		env.RECORDS.writeDataPoint({
 			'doubles': [temperature, humidity],
 		});
+	}
 
-		return Response.json({
-			version, nonce, error, temperature, humidity, signature: Array.from(new Uint8Array(signature))
-		});
-	},
-} satisfies ExportedHandler<Env>;
+	return new Response("OK");
+})
+
+export default { ...router }
