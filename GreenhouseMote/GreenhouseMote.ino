3d4c28217c6de5d7cd43e7170f350052a2968c53
John Hobbs <john@velvetcache.org>
2026-03-20 17:22:07 -0500

    Add TLS support

--- GreenhouseMote/GreenhouseMote.ino
+++ GreenhouseMote/GreenhouseMote.ino
@@ -1,10 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <AM2302-Sensor.h>
+#include <WiFiClientSecureBearSSL.h>
 #include <Crypto.h>
 #include <SHA256.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #include "config.h"
+#include "tls.h"
 
 struct SensorData {
   long version;
@@ -19,24 +23,10 @@ struct Packet {
   uint8_t hmac[SHA256::HASH_SIZE];
 };
 
-AM2302::AM2302_Sensor am2302{SENSOR_PIN};
+DHT_Unified dht(SENSOR_PIN, SENSOR_TYPE);
 
 SHA256 sha256;
 Hash *hash = &sha256;
-
-void connectToWifi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-}
 
 void setup() {
   Serial.begin(9600);
@@ -50,27 +40,45 @@ void setup() {
   pinMode(SENSOR_POWER_PIN, OUTPUT);
   digitalWrite(SENSOR_POWER_PIN, HIGH);
 
-  connectToWifi();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.println("Initializing AM2302 sensor...");
-  am2302.begin();
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Initializing sensor...");
+  dht.begin();
   delay(2000); // wait for sensor to prevent error on first read
 }
 
 void loop() {
+  auto certs = std::make_unique<BearSSL::X509List>(ROOT_CERT);
+  auto client = std::make_unique<BearSSL::WiFiClientSecure>();
+  client->setX509Time(1773869736); // TODO: Use NTP for the real time
+  client->setTrustAnchors(certs.get());
+
   SensorData data = {0x01, NAME, 0, 0.0, 0.0};
   Packet packet;
 
   Serial.println("Reading sensor...");
-  int8_t status = am2302.read();
-  Serial.print("Status:"); Serial.println(AM2302::AM2302_Sensor::get_sensorState(status));
-
-  if (status != AM2302::AM2302_READ_OK) {
-    data.error = status;
-  } else {
-    data.humidity = am2302.get_Humidity();
-    data.temperature = am2302.get_Temperature();
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
   }
+  data.temperature = event.temperature;
+
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  data.humidity = event.relative_humidity;
 
   Serial.print("Temperature = "); Serial.print(data.temperature); Serial.println(" °C");
   Serial.print("Humidity = "); Serial.print(data.humidity); Serial.println(" %");
@@ -87,16 +95,15 @@ void loop() {
   uint8_t packetBuffer[sizeof(Packet)];
   memcpy(packetBuffer, &packet, sizeof(Packet));
 
-  WiFiClient client;
-  HTTPClient http;
+  HTTPClient https;
 
   Serial.print("Posting data to server at " HTTP_HOST "...");
-  Serial.println(http.begin(client, "http://" HTTP_HOST "/update"));
-  int httpCode = http.POST(packetBuffer, sizeof(packetBuffer));
+  Serial.println(https.begin(*client, "https://" HTTP_HOST "/update"));
+  int httpCode = https.POST(packetBuffer, sizeof(packetBuffer));
   if (httpCode > 0) {
     Serial.printf("[HTTP] POST... code: %d\n", httpCode);
   } else {
-    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    Serial.printf("[HTTP] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
   }
 
   Serial.println("Going into deep sleep...");
